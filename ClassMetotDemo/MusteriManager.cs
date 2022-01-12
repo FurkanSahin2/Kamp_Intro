466dce024d59e8b6aeee3550c851486a473da55d
Furkan Şahin <aliince.000000@gmail.com>
2022-01-12 14:57:44 +0300

    12 Ocak 2022 Ders Sonu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        Musteri musteri = new Musteri();
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(" >--< Tebrikler Müşteri Eklendi >--< ");
+            Console.WriteLine("Ad: " + musteri.MusteriAd);
+            Console.WriteLine("Soyad: " + musteri.MusteriSoyad);
+            Console.WriteLine("Yas: " + musteri.Yas);
+            Console.WriteLine("Cinsiyet: " + musteri.Cinsiyet);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAd = "Ahmet";
+            musteri1.MusteriSoyad = "Yıldız";
+            musteri1.Yas = 25;
+            musteri1.Cinsiyet = "Erkek";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAd = "Merve";
+            musteri2.MusteriSoyad = "Korkmaz";
+            musteri2.Yas = 45;
+            musteri2.Cinsiyet = "Kadın";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriAd = "Veli";
+            musteri3.MusteriSoyad = "Şahin";
+            musteri3.Yas = 18;
+            musteri3.Cinsiyet = "Erkek";
+
+            Console.WriteLine("--------------- Müşteri Ekleme ---------------");
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Ekle(musteri1);
+            Console.WriteLine(" ");
+            musterimanager.Ekle(musteri2);
+            Console.WriteLine(" ");
+            musterimanager.Ekle(musteri3);
+            Console.WriteLine(" ");
+
+
+        }
+    }
+}
